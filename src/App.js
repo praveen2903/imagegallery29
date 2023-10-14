01d210c4ee43f7aac29c9e25ba7bc930d56ef96a
praveen2903 <vvit20bq1a05l0@gmail.com>
2023-10-14 18:44:51 +0530

    added pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Navbar from "./Components/NavbarDefault";
 import ImageCards from "./Components/ImageCard";
 import ImageCardDetails from "./Components/ImageCardDetails";
 import AddImage from "./Components/AddImage";
+import Login from "./Components/Login";
+import ImagesAdded from "./Components/ImagesAdded";
 
 export default function App() {
   return (
@@ -12,6 +14,8 @@ export default function App() {
         <Route path="/" element={<ImageCards/>}/>
         <Route path="/detailPage/:id" element={<ImageCardDetails/>}/>
         <Route path="/additions" element={<AddImage/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/addedphotos" element={<ImagesAdded/>} />
       </Routes>
     </div>
       
---
+++ src/Components/ImagesAdded.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ImagesAdded() {
+  return (
+    <div>ImagesAdded</div>
+  )
+}
+
+export default ImagesAdded
