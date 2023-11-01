c51feb7a3c977f3922649c77184db3c2d8038fc9
praveen2903 <vvit20bq1a05l0@gmail.com>
2023-11-01 20:37:28 +0530

    gallery option added

--- src/Components/addedimages/Card.jsx
+++ src/Components/addedimages/Card.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 
-function Card() {
+function Card({photoUrL,userphoto, name,title,description,address}) {
   return (
     <div>
       <div className="card w-96 bg-base-100 shadow-xl">
-        <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+        <figure><img src={photoUrL} alt="/" className='h-[200px] w-[200px]' /></figure>
         <div className="card-body">
-          <h2 className="card-title">
-            Shoes!
-            <div className="badge badge-secondary">NEW</div>
+          <h2 className="card-title gap-4">
+            <img src={userphoto} alt='/' className='rounded-full w-10'/>
+            {name}
           </h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <div className="badge badge-outline">Fashion</div> 
-            <div className="badge badge-outline">Products</div>
+          <p className='text-center font-semibold m-3'>{title}</p>
+          <p>{description}</p>
+          <div className="card-actions justify-end m-2">
+            <div className="badge badge-outline rounded-full bg-[#38bdf8] p-3 font-mono">{address}</div> 
           </div>
         </div>
       </div>
--- src/Components/addedimages/ImagesAdded.jsx
+++ src/Components/addedimages/ImagesAdded.jsx
@@ -1,13 +1,69 @@
 import React from 'react'
 import NavbarDefault from '../NavbarDefault'
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { collection, onSnapshot, query } from 'firebase/firestore';
+import { db } from '../../firebase/config';
+import Card from './Card';
+import { useContext } from 'react';
+import { AuthContext } from '../../context/AuthContext';
 
 const ImagesAdded = () => {
+  const {currentUser}=useContext(AuthContext)
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    let unsub;
+    const fetchData = async () => {
+      try {
+        const q = query(collection(db, 'userPhotos'));
+        unsub = onSnapshot(q, (querySnapshot) => {
+          const imageList = [];
+          querySnapshot.forEach((doc) => {
+            const data = doc.data();
+            imageList.push(data);
+          });
+          setImages(imageList);
+          // console.log(imageList)
+          setLoading(false);
+        });
+
+        return unsub;
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      unsub()
+    };
+  }, []);
+
+  // console.log(images[0].imageUrL)
+
   return (
     <div>
       <NavbarDefault/>
-      
+      {loading ? (
+        <div className='flex items-center justify-center'>
+          <span className="loading loading-spinner loading-lg text-info"></span>
+        </div>
+      ) : (
+        <div>
+          <div className='flex items-center justify-center text-3xl font-bold mb-10'>Uploaded Images</div>
+          <div className='grid gap-10 lg:grid-cols-3 md:grid-cols-2 mx-8 cursor-pointer'>
+            {images.map((image, index) => (
+            <Card key={index} photoUrL={image.imageUrL} name={image.name} userphoto={currentUser?.photoURL} title={image.title} description={image.description} address={currentUser?.email || currentUser?.phoneNumber}/>
+            ))}
+          </div>
+        </div>
+        
+      )}
     </div>
-  )
+  );
 }
 
 export default ImagesAdded
